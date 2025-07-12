e1dff02e3f87cfd1b6e6e3ab9987ed71721b714a
Wesley.S.Godoy <wpggodoi@gmail.com>
2025-07-12 19:35:47 -0300

    UI-implemented
    Added a functional play button
    
    Added a function that pauses the game when the player dies
    
    added a functional score

--- Flappy bird project/Assets/scripts/Game Manager.cs
+++ Flappy bird project/Assets/scripts/Game Manager.cs
@@ -1,16 +1,62 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private Player player;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private GameObject playButton;
+    [SerializeField] private GameObject gameOver;
+
+
+    //Score variable
     private int Score;
 
-    public void GameOver()
+    void Awake()
     {
-        Debug.Log("Game Over");
+
+        Application.targetFrameRate = 60;
+        Pause();
     }
 
+    public void Play()
+    {
+        Score = 0;
+        scoreText.text = Score.ToString();
+
+        playButton.SetActive(false);
+        gameOver.SetActive(false);
+
+        Time.timeScale = 1f;
+        player.enabled = true;
+        PipesMovement[] Pipes = FindObjectsByType<PipesMovement>(FindObjectsSortMode.None);
+
+        for (int i = 0; i < Pipes.Length; i++)
+        {
+            Destroy(Pipes[i].gameObject);
+        }
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        player.enabled = false;
+    }
+
+    //A method that stops and kill the game once the GameOver is called from the player's script
+    public void GameOver()
+    {
+        gameOver.SetActive(true);
+        playButton.SetActive(true);
+
+        Pause();
+    }
+
+    //A method to increase the score once the player scores
     public void IncreaseScore()
     {
         Score++;
+        scoreText.text = Score.ToString();
     } 
 }
