f20e808bd9f8d826064d6ddfb9c1d610f29a5934
Wesley.S.Godoy <wpggodoi@gmail.com>
2025-07-12 10:55:45 -0300

    Comments-in-player-script
    Added comments for a better understanding of the player script process

--- Flappy bird project/Assets/scripts/Player.cs
+++ Flappy bird project/Assets/scripts/Player.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 public class Player : MonoBehaviour 
 {
     private Rigidbody2D rb;
-    private bool wantsJump = false;
+    private bool Jump = false;
     public float jumpForce = 5f;
 
+    //starts before anything, sets the character RigidBody as the the component of 'rb' variable
     void Awake() => rb = GetComponent<Rigidbody2D>();
 
     void Update() {
+    //checks if the key "space" or the leftMouseButton were pressed, if its True, sets the jump variable to true
     if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
-        wantsJump = true;
+            Jump = true;
 }
 
     void FixedUpdate()
     {
-        if (wantsJump)
+        if (Jump)
         {
-            wantsJump = false;
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f); // reseta Y
+            //sets the jump to false and sets the vertical velocity to 0
+            Jump = false;
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f); 
+
+            //add a vertical instant impulse to the rigidBody 
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
     }
