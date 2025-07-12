9a53af80934a34f4c0faff288652401a18326efa
Wesley.S.Godoy <wpggodoi@gmail.com>
2025-07-12 15:05:52 -0300

    pipes-mechanic-done
    finished the pipes spawn and movement

--- Flappy bird project/Assets/scripts/PipesMovement.cs
+++ Flappy bird project/Assets/scripts/PipesMovement.cs
@@ -2,15 +2,32 @@ using UnityEngine;
 
 public class PipesMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+//Variable that determines the movement speed
+    [SerializeField] float speed = 5f;
+
+//Variable that determines the position where the prefab pipes will be destroyed
+    private float leftEdge;
+
+//When the script starts the variable leftEdge receives a position outside of the camera viewport
+    
     void Start()
     {
-        
+        leftEdge = Camera.main.ScreenToWorldPoint(Vector3.zero).x - 1f;
     }
 
-    // Update is called once per frame
-    void Update()
+
+//updates each frame
+    private void Update()
     {
-        
+        //moves the pipes to the left by changing its transform position sums the result of vector3.left * the speed variable * deltatime
+        transform.position += Vector3.left * speed * Time.deltaTime;
+
+        //if the transform.position in X is greater than the position of the leftEdge variable
+        if (transform.position.x < leftEdge)
+        {
+            // the object is destroyed
+            Destroy(gameObject);
+        }
     }
 }
--- Flappy bird project/Assets/scripts/Spawner.cs
+++ Flappy bird project/Assets/scripts/Spawner.cs
@@ -2,24 +2,39 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    //references the prefab as a gameObject
     public GameObject prefab;
 
+    //variable that determines the spawn rate
     [SerializeField] float spawnRate = 1f;
+
+    //variable that determines the minHeight to spawn the pipes pivot
     [SerializeField] float minHeight = -1f;
+
+    //variable that determines the maxHeight to spawn the pipes pivot
     [SerializeField] float maxHeight = 1f;
 
+
+//When the script is enabled
     private void OnEnable()
     {
-        InvokeRepeating(nameof(Spawn), spawnRate, spawnRate); 
+        //Begins to, repeatedly, call the Spawn method by giving its rate in seconds 
+        InvokeRepeating(nameof(Spawn), spawnRate, spawnRate);
     }
-
+//When the script is disabled
     void OnDisable()
     {
+        //Stops the repeat
         CancelInvoke(nameof(Spawn));   
     }
+
+    //the method that spawn the pipes
     private void Spawn()
     {
+        //The gameObject pipes are instantiated 
         GameObject pipes = Instantiate(prefab, transform.position, Quaternion.identity);
+
+        //then a random height is defined 
         pipes.transform.position += Vector3.up * Random.Range(minHeight, maxHeight);
     }
 
